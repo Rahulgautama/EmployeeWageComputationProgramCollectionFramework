1e82dbbe234d58ec28995e82a494f8016f1c72a8
Rahulgautama <rahul8874950727@gmail.com>
2023-05-10 23:00:54 +0530

    Calculate Employee Daily Wage

---
+++ EmpWageComputationUsingCollectionFramework/CalculateEmpDailyWage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpWageComputationUsingCollectionFramework
+{
+    class CalculateEmpDailyWage
+    {
+        public static List<int> EmpDailyWage(int emp_Rate_Per_Hrs)
+        {
+            int EMP_RAT_PER_HOUR = emp_Rate_Per_Hrs;
+            int IS_FULL_TIME = 1;
+
+            List<int> details = new List<int>();
+            details.Add(EMP_RAT_PER_HOUR);
+            details.Add(IS_FULL_TIME);
+            return details;
+        }
+        public void CalculateEmpWage()
+        {
+            var list = EmpDailyWage(20);
+            int empHour = 0,empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(2);
+            if (empCheck == list[1])
+            {
+                empHour = 8;
+            }
+            else
+            {
+                empHour = 0;
+            }
+            empWage = empHour * list[0];
+            Console.WriteLine("Employee Wage :" + empWage);
+        }
+    }
+}
--- EmpWageComputationUsingCollectionFramework/Program.cs
+++ EmpWageComputationUsingCollectionFramework/Program.cs
@@ -6,18 +6,9 @@ namespace EmpWageComputationUsingCollectionFramework
     {
         static void Main(string[] args)
         {
-            var data = new CheckAttendance();
-            var empData=data.EmpAttendance();
-            const int IS_FULL_TIME = 1;
-            Console.WriteLine("Welcome to Employee Wage Computation Program using Collection FrameWork");
-            if (empData[0]== IS_FULL_TIME)
-            {
-                Console.WriteLine("Empoyee is Present");
-            }
-            else
-            {
-                Console.WriteLine("Empoyee is Absent");
-            }
+            Console.WriteLine("Calculate Daily Employee Wage");
+            CalculateEmpDailyWage employeeDailyWage = new CalculateEmpDailyWage();
+            employeeDailyWage.CalculateEmpWage();
         }
     }
 }
