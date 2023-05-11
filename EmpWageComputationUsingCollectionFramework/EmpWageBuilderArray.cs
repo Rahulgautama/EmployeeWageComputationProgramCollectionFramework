68237a3446f26e6ba4c9e545b4027e282efdc716
Rahulgautama <rahul8874950727@gmail.com>
2023-05-11 16:10:36 +0530

    Calculate Employee Daily Wage Multiple Company

---
+++ EmpWageComputationUsingCollectionFramework/EmpWageBuilderArray.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpWageComputationUsingCollectionFramework
+{
+    public class EmpWageBuilderArray
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        private int numOfCompany = 0;
+        private CompanyEmployeeDailyWage[] companyEmpWageArray;
+
+        public EmpWageBuilderArray()
+        {
+            this.companyEmpWageArray = new CompanyEmployeeDailyWage[5];
+        }
+        public void AddCompanyEmpWage(string company, int empRatePerHours, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            companyEmpWageArray[this.numOfCompany] = new CompanyEmployeeDailyWage(company, empRatePerHours, numOfWorkingDays, maxHoursPerMonth);
+            numOfCompany++;
+        }
+        public void ComputeEmpWage()
+        {
+            for (int i = 0; i < numOfCompany; i++)
+            {
+                int val = this.ComputeEmpWage(this.companyEmpWageArray[i]);
+                companyEmpWageArray[i].setTotalEmpWage(val);
+                Console.WriteLine(this.companyEmpWageArray[i].toString());
+            }
+        }
+        public int ComputeEmpWage(CompanyEmployeeDailyWage companyEmployeeDailyWage)
+        {
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            while (totalEmpHrs <= companyEmployeeDailyWage.maxHoursPerMonth && totalWorkingDays < companyEmployeeDailyWage.numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                companyEmployeeDailyWage.totalEmpWage = totalEmpHrs * companyEmployeeDailyWage.empRatePerHour;
+                // companyEmployeeDailyWage.totalEmpWage += companyEmployeeDailyWage.totalEmpWage;
+
+            }
+
+
+            return companyEmployeeDailyWage.totalEmpWage;
+        }
+
+
+    }
+}
--- EmpWageComputationUsingCollectionFramework/Program.cs
+++ EmpWageComputationUsingCollectionFramework/Program.cs
@@ -7,21 +7,12 @@ namespace EmpWageComputationUsingCollectionFramework
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Calculate Daily Employee Wage");
+            EmpWageBuilderArray empWageBuilderArray = new EmpWageBuilderArray();
+            empWageBuilderArray.AddCompanyEmpWage("Flipkart", 20, 2, 10);
+            empWageBuilderArray.AddCompanyEmpWage("Amazon", 10, 4, 20);
+            empWageBuilderArray.AddCompanyEmpWage("Google", 32, 19, 8);
 
-            List<Model> data = new List<Model>{
-                new Model { companyName="Jio", maxHrsPerMonth=20, empRatePerHrs=20,empWage=0,totalEmpWage=0},
-                new Model { companyName="MI", maxHrsPerMonth=20,empRatePerHrs=20,empWage=0,totalEmpWage=0}
-                };
-
-            EmpWage employeeDailyWage = new EmpWage();
-            var empDetails = employeeDailyWage.CalculateWageFullTimePartTime(data);
-            foreach(var details in empDetails)
-            {
-                Console.WriteLine("Company Name is "+details.companyName+" Total Emp Wage ="+details.totalEmpWage);
-                
-            }
-           // Console.WriteLine("Employee Wage :" + empWage[]);
+            empWageBuilderArray.ComputeEmpWage();
         }
     }
 }
